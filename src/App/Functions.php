d6231ce96a1a04f70c6eb68024aabdee3972759b
Nicholas English <nenglish443@gmail.com>
2017-06-07 03:24:26 -0400

    Add some functions

--- src/App/Functions.php
+++ src/App/Functions.php
@@ -1,5 +1,5 @@
 <?php
-namespace App\Framework;
+namespace Valoon\App\Framework;
 defined('APP') || (header('HTTP/1.1 403 Forbidden') & die('403.14 - Directory listing denied.'));
 function redirect($url) {
 	$isExternal = stripos($url, "http://") !== false || stripos($url, "https://") !== false;
@@ -34,14 +34,14 @@ function e($value) {
 	return htmlentities($value, ENT_QUOTES, 'UTF-8', false);
 }
 function respond(array $data, $statusCode = 200) {
-    $response = new Response();
-    $response->send($data, $statusCode);
+	$response = new Response();
+	$response->send($data, $statusCode);
 }
 function app($service = null) {
 	$c = Container::getInstance();
 	if (is_null($service)) {
 		return $c;
 	}
-    return $c[$service];
+	return $c[$service];
 }
 ?>
